5ce1298670d6c2b2994772631de002ac493c59f8
rubanaaz <rubanaaz82@gmail.com>
2025-03-11 23:46:55 +0530

    Create DAY 24 LARGEST OF THREE NUMBERS USING IF-ELSE MOVEMENT.JAVA

---
+++ DAY 24 LARGEST OF THREE NUMBERS USING IF-ELSE MOVEMENT.JAVA
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int a, b, c;
+        Scanner sr = new Scanner(System.in);
+        a = sr.nextInt();
+        b = sr.nextInt();
+        c = sr.nextInt();
+        if(a>b && a>c) {
+            System.out.println(a);
+        }
+        else if(b>=a && b>=c) {
+            System.out.println(b);
+        }
+        else if(c>=a && c>b) {
+            System.out.println(c);
+        }
+        else if(a==0 && b==0 && c==0) {
+            System.out.println("0");
+        }
+    }
+}

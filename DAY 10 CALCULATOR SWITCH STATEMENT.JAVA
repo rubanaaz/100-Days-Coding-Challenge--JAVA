af1fcfbf4fafce1b579047eefe78d7efd8f967b5
rubanaaz <rubanaaz82@gmail.com>
2025-03-13 17:27:41 +0530

    Create DAY 10 CALCULATOR SWITCH STATEMENT.JAVA

---
+++ DAY 10 CALCULATOR SWITCH STATEMENT.JAVA
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        double num1 = scanner.nextDouble();
+        char operator = scanner.next().charAt(0); 
+        double num2 = scanner.nextDouble();
+
+        double result;
+
+        
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+            case '%':
+                if (num2 != 0) {
+                    result = num1 % num2;
+                } else {
+                    System.out.println("Error: Division by zero");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operator");
+                return;
+        }
+        System.out.println(result);
+    }
+}

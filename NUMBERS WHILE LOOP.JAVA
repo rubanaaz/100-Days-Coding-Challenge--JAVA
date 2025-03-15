634df6a94300ce53e0531f2bd28cc88e32b7907e
rubanaaz <rubanaaz82@gmail.com>
2025-03-15 23:18:18 +0530

    Create NUMBERS WHILE LOOP.JAVA

---
+++ NUMBERS WHILE LOOP.JAVA
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int i=0;
+        
+        while(i<10) {
+            i++;
+            System.out.println(i);
+        }
+    }
+}

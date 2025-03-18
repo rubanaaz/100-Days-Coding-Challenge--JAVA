863176caf73dcae106c315719aeafb2123211c35
rubanaaz <rubanaaz82@gmail.com>
2025-03-18 20:00:32 +0530

    Create DAY 15 Find All Prime Numbers in a Given Range Using a for Loop.JAVA

---
+++ DAY 15 Find All Prime Numbers in a Given Range Using a for Loop.JAVA
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+      
+        for (int i = start; i <= end; i++) {
+            int count = 0;
+            for (int j = 1; j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                }
+            }
+            if (count == 2) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+}

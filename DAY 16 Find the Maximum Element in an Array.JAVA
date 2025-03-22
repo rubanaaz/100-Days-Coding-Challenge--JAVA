00eb045c0f41d78fee1c13a2328113a856346592
rubanaaz <rubanaaz82@gmail.com>
2025-03-22 15:12:19 +0530

    Create DAY 16 Find the Maximum Element in an Array.JAVA

---
+++ DAY 16 Find the Maximum Element in an Array.JAVA
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        
+        int[] array = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        int maxElement = (n > array[0]) ? n : array[0];
+
+        for (int i = 1; i < n; i++) {
+            if (array[i] > maxElement) {
+                maxElement = array[i];
+            }
+        }
+        System.out.println(maxElement);
+    }
+}
